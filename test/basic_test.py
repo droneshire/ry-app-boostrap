1557008874e7ab951cc196814aacb7528997adb1
Ross Yeager <ryeager12@gmail.com>
2026-02-14 16:42:14 -0800

    style: make format/lint pass consistently

--- test/basic_test.py
+++ test/basic_test.py
@@ -5,4 +5,4 @@ from ry_app_bootstrap.version import get_version
 
 class BasicTest(unittest.TestCase):
     def test_missing_version_file_returns_empty(self) -> None:
-        self.assertEqual(get_version('/tmp/definitely-missing-version-file'), '')
+        self.assertEqual(get_version("/tmp/definitely-missing-version-file"), "")
